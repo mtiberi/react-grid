705a94b7718e7e8bc32d65b55a5a59bd0b32c8c1
mtiberi <mauro.tiberi@live.com>
2018-06-20 00:33:13 -0400

    cleanup

--- DataGridWithScrollBar.js
+++ DataGridWithScrollBar.js
@@ -37,13 +37,13 @@ class DataGridWithScrollBar extends React.Component {
         
         let props = {
             ...this.props,
-            width: pix(width -17),
+            width: pix(width - 17),
             height: pix(height - 2),
         }
 
         return <div className="react-grid outer-container" style={{ width: pix(width), height: pix(height) }} >
             <DataGrid setDataHeight={setDataHeight} scrollHandler={scrollHandler} {...props} />
-            <div className="react-grid scrollbar" style={{ height: props.height }}>
+            <div className="react-grid scrollbar" style={{ height: props.height }} >
                 <div ref={refScrollElement} style={{ width: '1px', height: '1px' }} >
                 </div>
             </div>
